8cbb4cbfc050cd1ab9435cc6b37b6c7eebd26e05
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2022-12-14 16:01:12 +0100

    Functional SurfaceFactor2 with test

---
+++ gtsam/slam/SurfaceFactor.h
@@ -0,0 +1,468 @@
+#pragma once
+
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/nonlinear/NonlinearFactor.h>
+
+namespace gtsam
+{
+  class GTSAM_EXPORT SurfaceFactor2
+      : public gtsam::NoiseModelFactor2<gtsam::Pose3, gtsam::Pose3>
+  {
+    gtsam::Point3 j0_; // In the frame of the first key
+    gtsam::Point3 l0_; // In the frame of the first key
+    gtsam::Point3 m0_; // In the frame of the first key
+    gtsam::Point3 i1_; // In the frame of the second key
+
+  public:
+    typedef gtsam::NoiseModelFactor2<gtsam::Pose3, gtsam::Pose3> Base;
+    typedef SurfaceFactor2 This;
+    typedef boost::shared_ptr<This> shared_ptr;
+
+    SurfaceFactor2(
+        const gtsam::Point3 &j0, const gtsam::Point3 &l0, const gtsam::Point3 &m0, gtsam::Key k0, const gtsam::Point3 &i1, gtsam::Key k1,
+        const gtsam::SharedNoiseModel &model)
+        : Base(model, k0, k1),
+          j0_(j0),
+          l0_(l0),
+          m0_(m0),
+          i1_(i1)
+    {
+    }
+
+    virtual ~SurfaceFactor2() {}
+
+    virtual gtsam::NonlinearFactor::shared_ptr clone() const override
+    {
+      return boost::static_pointer_cast<gtsam::NonlinearFactor>(
+          gtsam::NonlinearFactor::shared_ptr(new This(*this)));
+    }
+
+    /// Evaluate error h(x)-z and optionally derivatives
+    gtsam::Vector evaluateError(
+        const gtsam::Pose3 &p0, const gtsam::Pose3 &p1, boost::optional<gtsam::Matrix &> H0 = boost::none,
+        boost::optional<gtsam::Matrix &> H1 = boost::none) const override
+    {
+      gtsam::Matrix36 Hi, Hj, Hl, Hm;
+      bool compute_jacobians = H0 || H1;
+      gtsam::Point3 j_w = p0.transformFrom(j0_, compute_jacobians ? &Hj : 0);
+      gtsam::Point3 l_w = p0.transformFrom(l0_, compute_jacobians ? &Hl : 0);
+      gtsam::Point3 m_w = p0.transformFrom(m0_, compute_jacobians ? &Hm : 0);
+      gtsam::Point3 i_w = p1.transformFrom(i1_, compute_jacobians ? &Hi : 0);
+      gtsam::Vector3 n_w = (l_w - m_w).cross(j_w - m_w);
+      gtsam::Vector3 n_w_hat = n_w / n_w.norm();
+      gtsam::Vector3 residual = (i_w - m_w).dot(n_w_hat) * n_w_hat;
+
+      if (H0)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (gtsam::skewSymmetric(l_w - m_w) * (Hj - Hm) -
+                                     gtsam::skewSymmetric(j_w - m_w) * (Hl - Hm)) /
+                                    n_w.norm();
+
+        gtsam::Matrix36 temp =
+            ((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+            n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat - n_w_hat.transpose() * Hm);
+
+        *H0 = temp;
+      }
+
+      if (H1)
+      {
+        gtsam::Matrix36 temp = n_w_hat * n_w_hat.transpose() * Hi;
+
+        *H1 = temp;
+      }
+
+      return residual;
+    }
+  };
+
+  class GTSAM_EXPORT SurfaceFactor3JM
+      : public gtsam::NoiseModelFactor3<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3>
+  {
+    gtsam::Point3 j0_; // In the frame of the first key
+    gtsam::Point3 l1_; // In the frame of the second key
+    gtsam::Point3 m0_; // In the frame of the first key
+    gtsam::Point3 i2_; // In the frame of the third key
+
+  public:
+    typedef gtsam::NoiseModelFactor3<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3> Base;
+    typedef SurfaceFactor3JM This;
+    typedef boost::shared_ptr<This> shared_ptr;
+
+    SurfaceFactor3JM(
+        const gtsam::Point3 &j0, gtsam::Key k0, const gtsam::Point3 &l1, gtsam::Key k1,
+        const gtsam::Point3 &m0, const gtsam::Point3 &i2, gtsam::Key k2,
+        const gtsam::SharedNoiseModel &model)
+        : Base(model, k0, k1, k2),
+          j0_(j0),
+          l1_(l1),
+          m0_(m0),
+          i2_(i2)
+    {
+    }
+
+    virtual ~SurfaceFactor3JM() {}
+
+    virtual gtsam::NonlinearFactor::shared_ptr clone() const override
+    {
+      return boost::static_pointer_cast<gtsam::NonlinearFactor>(
+          gtsam::NonlinearFactor::shared_ptr(new This(*this)));
+    }
+
+    /// Evaluate error h(x)-z and optionally derivatives
+    gtsam::Vector evaluateError(
+        const gtsam::Pose3 &p0, const gtsam::Pose3 &p1, const gtsam::Pose3 &p2, boost::optional<gtsam::Matrix &> H0 = boost::none,
+        boost::optional<gtsam::Matrix &> H1 = boost::none,
+        boost::optional<gtsam::Matrix &> H2 = boost::none) const override
+    {
+      gtsam::Matrix36 Hi, Hj, Hl, Hm;
+      bool compute_jacobians = H0 || H1 || H2;
+      gtsam::Point3 j_w = p0.transformFrom(j0_, compute_jacobians ? &Hj : 0);
+      gtsam::Point3 l_w = p1.transformFrom(l1_, compute_jacobians ? &Hl : 0);
+      gtsam::Point3 m_w = p0.transformFrom(m0_, compute_jacobians ? &Hm : 0);
+      gtsam::Point3 i_w = p2.transformFrom(i2_, compute_jacobians ? &Hi : 0);
+      gtsam::Vector3 n_w = (l_w - m_w).cross(j_w - m_w);
+      gtsam::Vector3 n_w_hat = n_w / n_w.norm();
+      gtsam::Vector3 residual = (i_w - m_w).dot(n_w_hat) * n_w_hat;
+
+      if (H0)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (gtsam::skewSymmetric(l_w - m_w) * (Hj - Hm) -
+                                     gtsam::skewSymmetric(j_w - m_w) * (- Hm)) /
+                                    n_w.norm();
+
+        gtsam::Matrix36 temp =
+            ((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+            n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat - n_w_hat.transpose() * Hm);
+
+        *H0 = temp;
+      }
+
+      if (H1)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (-gtsam::skewSymmetric(j_w - m_w) * Hl) /
+                                    n_w.norm();
+
+        gtsam::Matrix36 temp =
+            ((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+            n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat);
+
+        *H1 = temp;
+      }
+
+      if (H2)
+      {
+        gtsam::Matrix36 temp = n_w_hat * n_w_hat.transpose() * Hi;
+
+        *H2 = temp;
+      }
+
+      return residual;
+    }
+  };
+
+  class GTSAM_EXPORT SurfaceFactor4
+      : public gtsam::NoiseModelFactor4<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3, gtsam::Pose3>
+  {
+    gtsam::Point3 j0_; // In the frame of the first key
+    gtsam::Point3 l1_; // In the frame of the second key
+    gtsam::Point3 m2_; // In the frame of the third key
+    gtsam::Point3 i3_; // In the frame of the fourth key
+
+    bool same_key_jl_;
+    bool same_key_lm_;
+    bool same_key_jm_;
+
+  public:
+    typedef gtsam::NoiseModelFactor4<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3, gtsam::Pose3> Base;
+    typedef SurfaceFactor4 This;
+    typedef boost::shared_ptr<This> shared_ptr;
+
+    SurfaceFactor4(
+        const gtsam::Point3 &j0, gtsam::Key k0, const gtsam::Point3 &l1, gtsam::Key k1,
+        const gtsam::Point3 &m2, gtsam::Key k2, const gtsam::Point3 &i3, gtsam::Key k3,
+        const gtsam::SharedNoiseModel &model)
+        : Base(model, k0, k1, k2, k3),
+          j0_(j0),
+          l1_(l1),
+          m2_(m2),
+          i3_(i3),
+          same_key_jl_(k0 == k1),
+          same_key_lm_(k1 == k2),
+          same_key_jm_(k0 == k2)
+    {
+    }
+
+    virtual ~SurfaceFactor4() {}
+
+    virtual gtsam::NonlinearFactor::shared_ptr clone() const override
+    {
+      return boost::static_pointer_cast<gtsam::NonlinearFactor>(
+          gtsam::NonlinearFactor::shared_ptr(new This(*this)));
+    }
+
+    /// Evaluate error h(x)-z and optionally derivatives
+    gtsam::Vector evaluateError(
+        const gtsam::Pose3 &p0, const gtsam::Pose3 &p1, const gtsam::Pose3 &p2,
+        const gtsam::Pose3 &p3, boost::optional<gtsam::Matrix &> H0 = boost::none,
+        boost::optional<gtsam::Matrix &> H1 = boost::none,
+        boost::optional<gtsam::Matrix &> H2 = boost::none,
+        boost::optional<gtsam::Matrix &> H3 = boost::none) const override
+    {
+      gtsam::Matrix36 Hi, Hj, Hl, Hm;
+      bool compute_jacobians = H0 || H1 || H2 || H3;
+      gtsam::Point3 j_w = p0.transformFrom(j0_, compute_jacobians ? &Hj : 0);
+      gtsam::Point3 l_w = p1.transformFrom(l1_, compute_jacobians ? &Hl : 0);
+      gtsam::Point3 m_w = p2.transformFrom(m2_, compute_jacobians ? &Hm : 0);
+      gtsam::Point3 i_w = p3.transformFrom(i3_, compute_jacobians ? &Hi : 0);
+      gtsam::Vector3 n_w = (l_w - m_w).cross(j_w - m_w);
+      gtsam::Vector3 n_w_hat = n_w / n_w.norm();
+      gtsam::Vector3 residual = (i_w - m_w).dot(n_w_hat) * n_w_hat;
+
+      const gtsam::Matrix36 Hi_3 = Hi;
+      const gtsam::Matrix36 Hj_0 = Hj;
+      gtsam::Matrix36 Hj_1, Hl_0;
+      if (same_key_jl_)
+      {
+        Hj_1 = Hj;
+        Hl_0 = Hl;
+      }
+      else
+      {
+        Hj_1 = gtsam::Matrix36::Zero();
+        Hl_0 = gtsam::Matrix36::Zero();
+      }
+      gtsam::Matrix36 Hj_2, Hm_0;
+      if (same_key_jm_)
+      {
+        Hj_2 = Hj;
+        Hm_0 = Hm;
+      }
+      else
+      {
+        Hj_2 = gtsam::Matrix36::Zero();
+        Hm_0 = gtsam::Matrix36::Zero();
+      }
+      const gtsam::Matrix36 Hl_1 = Hl;
+      gtsam::Matrix36 Hl_2, Hm_1;
+      if (same_key_lm_)
+      {
+        Hl_2 = Hl;
+        Hm_1 = Hm;
+      }
+      else
+      {
+        Hl_2 = gtsam::Matrix36::Zero();
+        Hm_1 = gtsam::Matrix36::Zero();
+      }
+      const gtsam::Matrix36 Hm_2 = Hm;
+
+      if (H0)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (gtsam::skewSymmetric(l_w - m_w) * (Hj_0 - Hm_0) -
+                                     gtsam::skewSymmetric(j_w - m_w) * (Hl_0 - Hm_0)) /
+                                    n_w.norm();
+
+        gtsam::Matrix36 temp =
+            ((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+            n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat - n_w_hat.transpose() * Hm_0);
+
+        *H0 = temp;
+      }
+
+      if (H1)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (gtsam::skewSymmetric(l_w - m_w) * (Hj_1 - Hm_1) -
+                                     gtsam::skewSymmetric(j_w - m_w) * (Hl_1 - Hm_1)) /
+                                    n_w.norm();
+
+        gtsam::Matrix36 temp =
+            ((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+            n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat - n_w_hat.transpose() * Hm_1);
+
+        *H1 = temp;
+      }
+
+      if (H2)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (gtsam::skewSymmetric(l_w - m_w) * (Hj_2 - Hm_2) -
+                                     gtsam::skewSymmetric(j_w - m_w) * (Hl_2 - Hm_2)) /
+                                    n_w.norm();
+
+        gtsam::Matrix36 temp =
+            ((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+            n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat - n_w_hat.transpose() * Hm_2);
+
+        *H2 = temp;
+      }
+
+      if (H3)
+      {
+        gtsam::Matrix36 temp = n_w_hat * n_w_hat.transpose() * Hi_3;
+
+        *H3 = temp;
+      }
+
+      return residual;
+    }
+  };
+
+  class GTSAM_EXPORT SurfaceFactor4S
+      : public gtsam::NoiseModelFactor4<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3, gtsam::Pose3>
+  {
+    gtsam::Point3 j0_; // In the frame of the first key
+    gtsam::Point3 l1_; // In the frame of the second key
+    gtsam::Point3 m2_; // In the frame of the third key
+    gtsam::Point3 i3_; // In the frame of the fourth key
+
+    bool same_key_jl_;
+    bool same_key_lm_;
+    bool same_key_jm_;
+
+  public:
+    typedef gtsam::NoiseModelFactor4<gtsam::Pose3, gtsam::Pose3, gtsam::Pose3, gtsam::Pose3> Base;
+    typedef SurfaceFactor4S This;
+    typedef boost::shared_ptr<This> shared_ptr;
+
+    SurfaceFactor4S(
+        const gtsam::Point3 &j0, gtsam::Key k0, const gtsam::Point3 &l1, gtsam::Key k1,
+        const gtsam::Point3 &m2, gtsam::Key k2, const gtsam::Point3 &i3, gtsam::Key k3,
+        const gtsam::SharedNoiseModel &model)
+        : Base(model, k0, k1, k2, k3),
+          j0_(j0),
+          l1_(l1),
+          m2_(m2),
+          i3_(i3),
+          same_key_jl_(k0 == k1),
+          same_key_lm_(k2 == k3),
+          same_key_jm_(k0 == k3)
+    {
+    }
+
+    virtual ~SurfaceFactor4S() {}
+
+    virtual gtsam::NonlinearFactor::shared_ptr clone() const override
+    {
+      return boost::static_pointer_cast<gtsam::NonlinearFactor>(
+          gtsam::NonlinearFactor::shared_ptr(new This(*this)));
+    }
+
+    /// Evaluate error h(x)-z and optionally derivatives
+    gtsam::Vector evaluateError(
+        const gtsam::Pose3 &p0, const gtsam::Pose3 &p1, const gtsam::Pose3 &p2,
+        const gtsam::Pose3 &p3, boost::optional<gtsam::Matrix &> H0 = boost::none,
+        boost::optional<gtsam::Matrix &> H1 = boost::none,
+        boost::optional<gtsam::Matrix &> H2 = boost::none,
+        boost::optional<gtsam::Matrix &> H3 = boost::none) const override
+    {
+      gtsam::Matrix36 Hi, Hj, Hl, Hm;
+      bool compute_jacobians = H0 || H1 || H2 || H3;
+      gtsam::Point3 j_w = p0.transformFrom(j0_, compute_jacobians ? &Hj : 0);
+      gtsam::Point3 l_w = p1.transformFrom(l1_, compute_jacobians ? &Hl : 0);
+      gtsam::Point3 m_w = p2.transformFrom(m2_, compute_jacobians ? &Hm : 0);
+      gtsam::Point3 i_w = p3.transformFrom(i3_, compute_jacobians ? &Hi : 0);
+      gtsam::Vector3 n_w = (l_w - m_w).cross(j_w - m_w);
+      gtsam::Vector3 n_w_hat = n_w / n_w.norm();
+      gtsam::Vector3 e_v = (i_w - m_w).dot(n_w_hat) * n_w_hat;
+      gtsam::Vector1 residual(e_v.norm());
+
+      const gtsam::Matrix36 Hi_3 = Hi;
+      const gtsam::Matrix36 Hj_0 = Hj;
+      gtsam::Matrix36 Hj_1, Hl_0;
+      if (same_key_jl_)
+      {
+        Hj_1 = Hj;
+        Hl_0 = Hl;
+      }
+      else
+      {
+        Hj_1 = gtsam::Matrix36::Zero();
+        Hl_0 = gtsam::Matrix36::Zero();
+      }
+      gtsam::Matrix36 Hj_2, Hm_0;
+      if (same_key_jm_)
+      {
+        Hj_2 = Hj;
+        Hm_0 = Hm;
+      }
+      else
+      {
+        Hj_2 = gtsam::Matrix36::Zero();
+        Hm_0 = gtsam::Matrix36::Zero();
+      }
+      const gtsam::Matrix36 Hl_1 = Hl;
+      gtsam::Matrix36 Hl_2, Hm_1;
+      if (same_key_lm_)
+      {
+        Hl_2 = Hl;
+        Hm_1 = Hm;
+      }
+      else
+      {
+        Hl_2 = gtsam::Matrix36::Zero();
+        Hm_1 = gtsam::Matrix36::Zero();
+      }
+      const gtsam::Matrix36 Hm_2 = Hm;
+
+      if (H0)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (gtsam::skewSymmetric(l_w - m_w) * (Hj_0 - Hm_0) -
+                                     gtsam::skewSymmetric(j_w - m_w) * (Hl_0 - Hm_0)) /
+                                    n_w.norm();
+
+        gtsam::Matrix16 temp =
+            (e_v.transpose() / e_v.norm()) *
+            (((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+             n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat - n_w_hat.transpose() * Hm_0));
+
+        *H0 = temp;
+      }
+
+      if (H1)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (gtsam::skewSymmetric(l_w - m_w) * (Hj_1 - Hm_1) -
+                                     gtsam::skewSymmetric(j_w - m_w) * (Hl_1 - Hm_1)) /
+                                    n_w.norm();
+
+        gtsam::Matrix16 temp =
+            (e_v.transpose() / e_v.norm()) *
+            (((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+             n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat - n_w_hat.transpose() * Hm_1));
+
+        *H1 = temp;
+      }
+
+      if (H2)
+      {
+        gtsam::Matrix36 d_n_w_hat = (gtsam::Matrix33::Identity() - n_w_hat * n_w_hat.transpose()) *
+                                    (gtsam::skewSymmetric(l_w - m_w) * (Hj_2 - Hm_2) -
+                                     gtsam::skewSymmetric(j_w - m_w) * (Hl_2 - Hm_2)) /
+                                    n_w.norm();
+
+        gtsam::Matrix16 temp =
+            (e_v.transpose() / e_v.norm()) *
+            (((i_w - m_w).dot(n_w_hat)) * d_n_w_hat +
+             n_w_hat * ((i_w - m_w).transpose() * d_n_w_hat - n_w_hat.transpose() * Hm_2));
+
+        *H2 = temp;
+      }
+
+      if (H3)
+      {
+        gtsam::Matrix16 temp = (e_v.transpose() / e_v.norm()) * n_w_hat * n_w_hat.transpose() * Hi_3;
+
+        *H3 = temp;
+      }
+
+      return residual;
+    }
+  };
+} // namespace gtsam
---
+++ gtsam/slam/tests/testSurfaceFactor.cpp
@@ -0,0 +1,90 @@
+#include <gtsam/slam/SurfaceFactor.h>
+#include <gtsam/inference/Symbol.h>
+#include <gtsam/nonlinear/Values.h>
+#include <gtsam/nonlinear/factorTesting.h>
+#include <gtsam/nonlinear/ISAM2.h>
+
+#include <CppUnitLite/TestHarness.h>
+
+using namespace gtsam;
+
+using symbol_shorthand::X; // Pose3 (x,y,z,r,p,y)
+
+std::mt19937 mt(42);
+size_t max_points = 10;
+double max_edge_len = 0.5;
+
+void getOptimizedError(const NonlinearFactorGraph & graph, const Values& initial, const Values& ground_truth, Pose3 & p1_error, Pose3 & p2_error)
+{
+  ISAM2 isam2;
+  isam2.update(graph, initial);
+  Values result = isam2.calculateEstimate();
+  Pose3 optimized_p1 = result.at<Pose3>(X(1));
+  Pose3 optimized_p2 = result.at<Pose3>(X(2));
+  p1_error = optimized_p1.between(ground_truth.at<Pose3>(X(1)));
+  p2_error = optimized_p2.between(ground_truth.at<Pose3>(X(2)));
+}
+
+// Random points random poses
+TEST(SurfaceFactor2, JacobiansAndOpt1)
+{
+  Pose3 p1(Rot3::Random(mt, M_PI/20.0), Point3::Random());
+  Pose3 p2(Rot3::Random(mt, M_PI/20.0), Point3::Random());
+
+  NonlinearFactorGraph graph;
+  graph.addPrior<Pose3>(X(1), p1, noiseModel::Unit::Create(6));
+
+  Values values;
+  values.insert(X(1), p1);
+  values.insert(X(2), p2);
+
+  for (size_t iter = 0; iter < max_points; ++iter)
+  {
+    Point3 j_w = Point3::Random();
+    Vector3 dv1_w = Vector3::Random();
+    dv1_w.normalize();
+    double edge_len1 = rand() / static_cast<double>(RAND_MAX) * max_edge_len;
+    Point3 l_w = j_w + edge_len1 * dv1_w;
+    Vector3 dv2_w = Vector3::Random();
+    dv2_w.normalize();
+    double edge_len2 = rand() / static_cast<double>(RAND_MAX) * max_edge_len;
+    Point3 m_w = j_w + edge_len2 * dv2_w;
+    float lambda1 = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    float lambda2 = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    Point3 i_w = j_w + lambda1 * dv1_w + lambda2 * dv2_w;
+    Point3 i = p2.transformTo(i_w) + Point3::Random().normalized() * 0.00001;
+
+    // Reorder points to make sure that m_w is the furthest point
+    if ((i_w - j_w).norm() > (i_w - m_w).norm())
+      std::swap(j_w, m_w);
+    if ((i_w - l_w).norm() > (i_w - m_w).norm())
+      std::swap(l_w, m_w);
+
+    // Reorder j_w and l_w to make sure that j_w is the closest point - This is not a requirement as the factor is designed. But this should be tested if jacobians ever start becoming wrong.
+    // if ((i_w - j_w).norm() > (i_w - l_w).norm())
+    //   std::swap(j_w, l_w);
+
+    // Convert points j, l and m to their local frames
+    Point3 j = p1.transformTo(j_w);
+    Point3 l = p1.transformTo(l_w);
+    Point3 m = p1.transformTo(m_w);
+
+    SurfaceFactor2 factor(j, l, m, X(1), i, X(2), noiseModel::Unit::Create(3));
+    EXPECT_CORRECT_FACTOR_JACOBIANS(factor, values, 1e-5, 1e-5);
+    graph.add(factor);
+  }
+
+  Pose3 p1_error, p2_error;
+  getOptimizedError(graph, values, values, p1_error, p2_error);
+  EXPECT(assert_equal(0.0, p1_error.translation().norm(), 1e-2));
+  EXPECT(assert_equal(0.0, p2_error.translation().norm(), 1e-2));
+  EXPECT(assert_equal(0.0, p1_error.rotation().axisAngle().second, 1e-2));
+  EXPECT(assert_equal(0.0, p2_error.rotation().axisAngle().second, 1e-2));
+}
+
+
+int main()
+{
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
